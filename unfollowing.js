77a822bd2dc024daedb3ecb6c37178f9bace78e5
sedgwickz <kunnsh@gmail.com>
2021-03-15 18:33:51 +0800

    Update unfollowing.js

--- unfollowing.js
+++ unfollowing.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://twitter.com/sedgwickz/following
+// @match        https://twitter.com/*/following
 // @grant        none
 // ==/UserScript==
 
